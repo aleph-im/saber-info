82b36d70afa93ef4febf5552bca6a1c881a2551b
Moshe Malawach <moshe.malawach@protonmail.com>
2021-09-30 12:49:32 +0200

    force store boot

--- quasar.conf.js
+++ quasar.conf.js
@@ -22,7 +22,8 @@ module.exports = configure(function (ctx) {
     boot: [
       // 'i18n',
       'axios',
-      'apexcharts'
+      'apexcharts',
+      'store'
     ],
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
